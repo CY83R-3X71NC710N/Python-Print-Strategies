07cb605a1ccd827625fddf0b11aa5446b5b75f9d
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-02-11 14:56:17 -0800

    Create print.py

---
+++ print.py
@@ -0,0 +1,27 @@
+data = "Fun secret variable"
+print (data)
+print("Hello World")
+print(50)
+print(1000)
+print(3.142)
+print(50, 1000, 3.142, "Hello World") # Comma separation
+
+# printing on same line
+print("Hello", end="")
+print("World")
+
+print("Hello", end=" ")
+print("World")
+
+print(50)  # This line prints 50
+print("Hello World")  # This line prints Hello World
+
+# This is just a comment hanging out on its own!
+
+# For multi-line comments, we must
+# add the hashtag symbol
+# each time
+
+""" Docstrings are pretty cool
+for writing longer comments
+or notes about the code"""
